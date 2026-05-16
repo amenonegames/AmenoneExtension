d07b7e4db181e18bf28a9af8a1605a36d447c403
amenonegames <100146683+amenonegames@users.noreply.github.com>
2026-05-16 20:54:06 +0900

    textmeshpro extension

---
+++ Packages/com.amenone.unityextension/Scripts/TMPExtension.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public static class TMPExtension
+    {
+        public static void ClearVertexColors(this TMP_Text textComponent , Color initialCol)
+        {
+            textComponent.ForceMeshUpdate(true);
+            var textInfo = textComponent.textInfo;
+
+            var count = Mathf.Min(textInfo.characterCount, textInfo.characterInfo.Length);
+            for (int i = 0; i < count; i++)
+            {
+                var charInfo = textInfo.characterInfo[i];
+                if (!charInfo.isVisible)
+                    continue;
+
+                int materialIndex = charInfo.materialReferenceIndex;
+                int vertexIndex = charInfo.vertexIndex;
+
+                Color32[] colors = textInfo.meshInfo[materialIndex].colors32;
+
+                colors[vertexIndex + 0] = initialCol;
+                colors[vertexIndex + 1] = initialCol;
+                colors[vertexIndex + 2] = initialCol;
+                colors[vertexIndex + 3] = initialCol;
+            }
+
+            for (int i = 0; i < textInfo.materialCount; i++)
+            {
+                if (textInfo.meshInfo[i].mesh == null) { continue; }
+
+                textInfo.meshInfo[i].mesh.colors32 = textInfo.meshInfo[i].colors32;
+                textComponent.UpdateGeometry(textInfo.meshInfo[i].mesh, i);
+            }
+        }
+    }
+}
